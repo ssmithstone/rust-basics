afff4bfcac9f1527332f6caf6d784f505d7603a1
Stephen Smithstone <stephen.smithstone@digitaslbi.com>
2020-08-22 16:24:40 +1000

    read any size line from stdin and print to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
+use std::io;
+
 fn main() {
     println!("Learning Programming Languages");
     println!("rust-basics");
+
+    println!("Reading from stdin");
+    let mut buffer = String::new();
+
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line");
+
+
+    println!("User entered {}" , buffer);
+
+
 }
