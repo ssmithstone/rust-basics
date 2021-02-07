04fbf3caf7b4e7913bcb88f18bdd1453a0d4cd0f
Stephen Smithstone <ssmithstone@me.com>
2021-02-07 17:21:11 +1100

    basic int var , imutable and mutable

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use std::io;
-
 fn main() {
     println!("Learning Programming Languages");
     println!("rust-basics");
 
+    let a: u8 = 123; // bind a to value 123 8bits , 1byte. u = unsigned (positive number) , signed ( negative / positive )
+    let b: i8 = -127; // signed 8bit value -127 - 128
 
+    println!("a = {} , b = {}", a, b); // print macro with value interpolation.
 
+    let mut c: i16 = 0; //mutable value
+    println!("c = {}", c);
+    c = 42;
+    println!("c = {}", c);
 }
