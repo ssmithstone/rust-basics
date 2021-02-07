3187597e7cb9c3c9f810cae26fe6abdfe88bbe9e
Stephen Smithstone <ssmithstone@me.com>
2021-02-07 16:53:58 +1100

    Remove unwanted lines to rest learning

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,6 @@ fn main() {
     println!("Learning Programming Languages");
     println!("rust-basics");
 
-    println!("Reading from stdin");
-    let mut buffer = String::new();
 
-    io::stdin()
-        .read_line(&mut buffer)
-        .expect("Failed to read line");
-
-    let res = buffer.trim().parse::<u32>();
-    let res = match res {
-        Ok(res) => res,
-        Err(error) => panic!("Error parsing {:?}" , error),
-    };
-
-    let res2 : u32 = buffer.trim().parse().unwrap();
-    let res3 : u32 = buffer.trim().parse().expect("Error parsing ");
-
-    println!("User entered {} {} {}" , res , res2, res3);
 
 }
